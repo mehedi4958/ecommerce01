77b2154e6e4fb71946def671490679caadedb2d8
Mehedi <mehedi@augnitive.com>
2022-11-09 22:50:13 +0600

    sign up method created using firebase authentication

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+///colors
 var backgroundColor = Colors.black;
 var buttonColor = Colors.black;
 var buttonTextColor = Colors.white;
+
+///Firebase
+var firebaseAuth = FirebaseAuth.instance;
---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce_01/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthController {
+  ///function to sign up users
+  Future<String> signUpUser(
+      String fullName, String username, String email, String password) async {
+    String result = 'some error occured';
+    try {
+      if (fullName.isNotEmpty &&
+          username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty) {
+        UserCredential userCredential = await firebaseAuth
+            .createUserWithEmailAndPassword(email: email, password: password);
+        print(userCredential.user!.email);
+        result = 'success';
+      } else {
+        result = 'Fields must not be empty';
+      }
+    } catch (e) {
+      result = e.toString();
+    }
+    return result;
+  }
+}
