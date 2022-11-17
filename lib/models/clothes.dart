53b3b1b97fd73243f5502f8ea0656246b7736c87
Mehedi <mehedi@augnitive.com>
2022-11-17 23:58:47 +0600

    mock clothes model created

---
+++ lib/models/clothes.dart
@@ -0,0 +1,22 @@
+class Clothes {
+  Clothes(this.productName, this.price, this.imageUrl);
+
+  String? productName;
+  String? price;
+  String? imageUrl;
+
+  static List<Clothes> generateClothes() {
+    return [
+      Clothes(
+        'Gucci oversize',
+        '\$70.99',
+        'assets/images/arrival1.png',
+      ),
+      Clothes(
+        'T-Shirt oversize',
+        '\$80.99',
+        'assets/images/arrival2.png',
+      ),
+    ];
+  }
+}
