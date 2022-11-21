0c49513293396bbde91a2f602dfcad5d50b6528f
Mehedi <mehedi@augnitive.com>
2022-11-21 16:49:02 +0600

    backward button provided to detail screen

--- lib/views/screens/detail/detail_screen.dart
+++ lib/views/screens/detail/detail_screen.dart
@@ -25,6 +25,26 @@ class DetailScreen extends StatelessWidget {
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
+              Positioned(
+                left: 15,
+                top: 35,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ],
