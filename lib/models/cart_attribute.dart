421c88b0711975826d6e6113aae72d1e629c147c
Mehedi <mehedi@augnitive.com>
2022-11-24 20:15:30 +0600

    cart attribute model created

---
+++ lib/models/cart_attribute.dart
@@ -0,0 +1,15 @@
+class CartAttribute {
+  CartAttribute({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+    required this.imageUrl,
+  });
+
+  final String id;
+  final String title;
+  final String quantity;
+  final double price;
+  final String imageUrl;
+}
