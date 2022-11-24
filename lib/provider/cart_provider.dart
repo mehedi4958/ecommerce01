f30b7d6f367b9ab098847c7a5cc8f9376980f53f
Mehedi <mehedi@augnitive.com>
2022-11-24 20:47:42 +0600

    function to calculated price of the cart items added

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -7,4 +7,12 @@ class CartProvider with ChangeNotifier {
   Map<String, CartAttribute> get getCartItems {
     return {..._cartItems};
   }
+
+  double get totalAmount {
+    var total = 0.0;
+    _cartItems.forEach((key, value) {
+      total += value.price * value.quantity;
+    });
+    return total;
+  }
 }
