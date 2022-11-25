167925aa89b8cfd0abe38ad20ec53a97040e98b2
Mehedi <mehedi@augnitive.com>
2022-11-25 08:13:58 +0600

    shop now button added to empty cart

--- lib/views/widgets/cart_empty.dart
+++ lib/views/widgets/cart_empty.dart
@@ -5,26 +5,50 @@ class CartEmpty extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height * 0.4,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/emptycart.png'),
+    return Container(
+      margin: const EdgeInsets.only(top: 90),
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.4,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/emptycart.png'),
+              ),
             ),
           ),
-        ),
-        const Text(
-          'Your Cart is Empty',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
+          const Text(
+            'Your Cart is Empty',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
           ),
-        ),
-      ],
+          const SizedBox(height: 15),
+          SizedBox(
+            width: MediaQuery.of(context).size.width - 40,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: () {},
+              child: const Center(
+                child: Text(
+                  'Shop now',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
