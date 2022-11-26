743894ff3bdcafc44d81c1fd79f87da9f961df7f
Mehedi <mehedi@augnitive.com>
2022-11-26 13:29:33 +0600

    data fetching from firestore to profile screen

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -1,12 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce_01/constants.dart';
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String? _name;
+  String? _email;
+
+  void getUserData() async {
+    DocumentSnapshot userData = await fireStore
+        .collection('users')
+        .doc(firebaseAuth.currentUser!.uid)
+        .get();
+
+    setState(() {
+      _name = userData.get('username');
+      _email = userData.get('email');
+    });
+  }
+
+  @override
+  void initState() {
+    getUserData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('$_name'),
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () async {
