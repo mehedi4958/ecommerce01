d242a62092bf1fe9a01920deebeee2563d418427
Mehedi <mehedi@augnitive.com>
2022-11-27 21:46:52 +0600

    product searching using provider

--- lib/views/screens/search_screen.dart
+++ lib/views/screens/search_screen.dart
@@ -1,16 +1,31 @@
+import 'package:e_commerce_01/models/products.dart';
+import 'package:e_commerce_01/provider/products.dart';
+import 'package:e_commerce_01/views/widgets/feeds_products.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  List<Product> _searchedList = [];
+
+  @override
   Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+
     return Scaffold(
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.black),
         elevation: 1,
         backgroundColor: Colors.white,
         title: TextField(
+          controller: _searchController,
           decoration: InputDecoration(
             fillColor: Colors.white,
             filled: true,
@@ -27,16 +42,30 @@ class SearchScreen extends StatelessWidget {
                 width: 15,
               ),
             ),
-            suffixIcon: const Icon(Icons.clear),
+            suffixIcon: GestureDetector(
+                onTap: () {
+                  _searchController.clear();
+                },
+                child: const Icon(Icons.clear)),
           ),
+          onChanged: (value) {
+            _searchController.text.toLowerCase();
+            setState(() {
+              _searchedList = productData.searchQuery(_searchController.text);
+            });
+          },
         ),
       ),
-      body: const Center(
-        child: Text(
-          'Search a product',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+      body: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 240 / 320,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        children: List.generate(
+          _searchedList.length,
+          (index) => ChangeNotifierProvider.value(
+            value: _searchedList[index],
+            child: const FeedsProducts(),
           ),
         ),
       ),
